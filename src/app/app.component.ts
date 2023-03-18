32fee2063bece18010b897464a22acd8af267065
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-18 10:45:23 +0300

    add app componentt entry point

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'in-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
