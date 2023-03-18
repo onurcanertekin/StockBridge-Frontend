883b25a0f0c96550ffb314f88c47b6ec25db0140
onurcan ertekin <onrcn_ertkn@hotmail.com>
2023-03-18 11:12:00 +0300

    add layouts module for all nav releated components

---
+++ src/app/common/common.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { LayoutsComponent } from "./layouts/layouts.component";
+import { LayoutsModule } from "./layouts/layouts.module";
+
+@NgModule({
+  declarations: [
+    LayoutsComponent
+  ],
+  imports: [
+    LayoutsModule
+  ],
+  exports: [
+
+  ]
+})
+export class CommonModule { }
